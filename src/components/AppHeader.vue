e2e6710cf0c55c2f7f2b755b3e78dfa7aa0b8e1a
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-11-18 17:15:41 +0100

    added route to AppProjects

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -29,6 +29,11 @@ export default {
                                 About
                             </router-link>
                         </li>
+                        <li class="nav-item">
+                            <router-link :to="{ name: 'projects' }">
+                                Projects
+                            </router-link>
+                        </li>
                     </ul>
                 </div>
             </div>
