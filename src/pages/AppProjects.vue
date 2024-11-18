cb5142f5b22cde045a3d58144aad0a8093a84592
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-11-18 17:15:03 +0100

    import ProjectsList

---
+++ src/pages/AppProjects.vue
@@ -0,0 +1,24 @@
+<script>
+
+import ProjectsList from "../components/ProjectsList.vue";
+
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        ProjectsList
+    }
+}
+</script>
+
+<template>
+    <section id="Projects">
+        <ProjectsList />
+    </section>
+</template>
+
+<style lang="scss" scoped></style>
