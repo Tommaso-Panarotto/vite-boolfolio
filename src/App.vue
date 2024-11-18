3b53f61c0c4cd4fefec88b0d721ef085b5d0717b
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-11-18 16:53:42 +0100

    added AppHeader and RouterView

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
-import AppMain from "./components/AppMain.vue";
+import AppHeader from './components/AppHeader.vue';
+
 
 export default {
   data() {
@@ -8,13 +9,14 @@ export default {
     }
   },
   components: {
-    AppMain
+    AppHeader
   }
 }
 </script>
 
 <template>
-  <AppMain />
+  <AppHeader />
+  <RouterView />
 </template>
 
 <style lang="scss">
