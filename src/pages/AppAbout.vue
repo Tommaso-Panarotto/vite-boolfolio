3ad3f461dab3ae6a06b4f0849410f2fec9a5d26e
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-11-18 16:52:09 +0100

    AppAbout

---
+++ src/pages/AppAbout.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container text-center">
+        <h1 class="mt-5">AppAbout</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia nulla eius dignissimos sequi esse dolorum
+            facere. Obcaecati quaerat expedita voluptatum, reiciendis commodi quo rerum nihil. Id eaque inventore
+            perspiciatis molestias?</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+h2 {
+    color: red;
+}
+</style>
